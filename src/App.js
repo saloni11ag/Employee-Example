c7cdcb914a6847f6e148d7e02ee5d21e2d080751
Saloni Agrawal <Saloni.Agrawal@cognizant.com>
2020-08-05 13:36:14 +0530

    adding data to table using axios.get

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Employee from './Employee';
+import axios from 'axios';
 
-function App() {
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      employee: []
+     }
+}
+  componentWillMount() {
+    this.getData()
+  }
+
+  getData = () => {
+    axios.get('http://dummy.restapiexample.com/api/v1/employees')
+    .then(
+      response=>{
+        this.setState({employee: response.data.data})
+        console.log(this.state.employee);
+    }, error=>{
+        console.error(error);
+    }
+    )
+  }
+  getEmployee = () => {
+    console.log("hii");
+    return this.state.employee.map(emp => {
+      return (
+        <Employee
+        key = {emp.id}
+        id = {emp.id}
+        name = {emp.employee_name}
+        salary = {emp.employee_salary}
+        age = {emp.employee_age}
+        ></Employee>
+      )
+    })
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Salary</th>
+          <th>Age</th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.getEmployee()}
+      </tbody>
+    </table>
     </div>
   );
+  }
 }
 
 export default App;
